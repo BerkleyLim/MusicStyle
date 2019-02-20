55a8b9268394f3a79a86eeac0bf2c4521504bfe9
berkleylim <berkleylim16@gmail.com>
2019-02-20 19:33:14 +0900

    웹 구축 환경설정 세팅

---
+++ src/main/java/com/musicstyle/www/author/hyunsik/lim/pms/domain/Board.java
@@ -0,0 +1,34 @@
+package bitcamp.java106.pms.domain;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.Arrays;
+
+public class Board implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    // private int no;
+    // private int memno;
+    // private Date createDate;
+    // private String content;
+    // private String path;
+    // private int count;
+    // private int like;
+    // private int commtCnt;
+    // private String[] boardhashtag;
+    // private int totalCnt;
+    
+    // private String profile;
+    
+    // private String hashtag;
+    // private String nickname;
+    // private String comments;
+    
+    // private String product;
+    // private String address;
+    // private int poX;
+    // private int poY;
+    // private int lno;
+    
+
+}
