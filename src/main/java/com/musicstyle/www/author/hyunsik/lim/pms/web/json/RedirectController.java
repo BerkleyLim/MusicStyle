285afe5585a7963a0562e2841c73f71bd6459d9f
berkleylim <berkleylim16@gmail.com>
2019-05-18 23:22:09 +0900

    페이지 이동

--- src/main/java/com/musicstyle/www/author/hyunsik/lim/pms/web/json/RedirectController.java
+++ src/main/java/com/musicstyle/www/author/hyunsik/lim/pms/web/json/RedirectController.java
@@ -10,31 +10,45 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 @RequestMapping("/")
 public class RedirectController {
-//    
-//    @Autowired ServletContext sc;
-//    
     @GetMapping("/information")
     public ModelAndView movedInformation(ModelMap model) {
-//        model.addAttribute("attribute", "redirectWithRedirectPrefix");
-        //model.addAttribute("attribute", "redirectWithRedirectPrefix");
-        //return new ModelAndView("redirect:/redirectedUrl", model);
         return new ModelAndView("forward:/information/information.html");
     }
     
     @GetMapping("/sheetmusic")
     public ModelAndView movedSheetMusic(ModelMap model) {
-//        model.addAttribute("attribute", "redirectWithRedirectPrefix");
-        //model.addAttribute("attribute", "redirectWithRedirectPrefix");
-        //return new ModelAndView("redirect:/redirectedUrl", model);
         return new ModelAndView("forward:/sheetmusic/sheetmusic.html");
     }
     
     @GetMapping("/media")
+    public ModelAndView movedMedia(ModelMap model) {
+        return new ModelAndView("forward:/media/media.html");
+    }
+    
+    @GetMapping("/introduction")
+    public ModelAndView movedIntroduction(ModelMap model) {
+//        model.addAttribute("attribute", "redirectWithRedirectPrefix");
+        //model.addAttribute("attribute", "redirectWithRedirectPrefix");
+        //return new ModelAndView("redirect:/redirectedUrl", model);
+        return new ModelAndView("forward:/introduction/introduction.html");
+    }
+    
+    @GetMapping("/practiceroom")
+    public ModelAndView movedPrecticeRoom(ModelMap model) {
+        return new ModelAndView("forward:/practiceRoom/practiceRoom.html");
+    }
+    
+    @GetMapping("/teacher")
+    public ModelAndView movedTeacher(ModelMap model) {
+        return new ModelAndView("forward:/teacher/teacher.html");
+    }
+    
+    @GetMapping("/community")
     public ModelAndView redirectWithUsingRedirectPrefix(ModelMap model) {
 //        model.addAttribute("attribute", "redirectWithRedirectPrefix");
         //model.addAttribute("attribute", "redirectWithRedirectPrefix");
         //return new ModelAndView("redirect:/redirectedUrl", model);
-        return new ModelAndView("forward:/media/media.html");
+        return new ModelAndView("forward:/community/community.html");
     }
     
 }
