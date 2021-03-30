e9520d23514634d46309fb2266a64d7b4ef52caf
berkleylim <berkleylim16@gmail.com>
2021-03-30 15:27:46 +0900

    To support Korean viewer

--- src/main/java/com/musicstyle/www/author/hyunsik/lim/pms/web/json/PracticeRoomController.java
+++ src/main/java/com/musicstyle/www/author/hyunsik/lim/pms/web/json/PracticeRoomController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/practiceroom")
+@RequestMapping("/practiceRoom")
 public class PracticeRoomController {
 //    
 //    @Autowired ServletContext sc;
