1f89e6a8ca6fa7e0f87a77b7a5a76cc01cfa8677
berkleylim <berkleylim16@gmail.com>
2021-05-31 16:34:27 +0900

    information backend

--- src/main/java/com/musicstyle/www/author/hyunsik/lim/pms/domain/Information.java
+++ src/main/java/com/musicstyle/www/author/hyunsik/lim/pms/domain/Information.java
@@ -6,21 +6,13 @@ import java.sql.Date;
 public class Information implements Serializable {
     private static final long serialVersionUID = 1L;
     
-    private long no;
-    private int memno;
-    private Date createDate;
+    private long ino;
+    private String username;
+    private String titie;
+    private String img;
+    private String genre;
+    private String releaseDate;
+    private String musicLink;
     private String content;
-    private String path;
-    private int count;
-    private int like;
-    private int commtCnt;
-    private String[] boardhashtag;
-    private int totalCnt;
-    
-    // private String profile;
-    
-    // private String hashtag;
-    // private String nickname;
-    // private String comments;
     
 }
