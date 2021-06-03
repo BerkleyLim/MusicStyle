64035233baad2283f4b1add1394681e6facbeb0f
berkleylim <berkleylim16@gmail.com>
2021-06-03 21:05:01 +0900

    information sql

---
+++ information insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `INFORMATION`
+    (`no`, `username`, `view`, `artist`, `title`, `img`, `genre`, `release_date`, `album_name`, `music_link`, `content`, `upload_date`) 
+    VALUES (1,'berkley', 0, 'Yiruma', 'Yellow moon', '/image/sample-yiruma-chaconne.png', 'newAge', 1998, 'Yellow moon', '', '이곡은 클래식을 기반으로 하여 작성하였다.'
+    ,now());
