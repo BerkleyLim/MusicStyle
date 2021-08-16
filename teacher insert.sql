c4e4b84c95d3dce90b358954e11054efbb6c4694
berkleylim <berkleylim16@gmail.com>
2021-08-16 10:41:06 +0900

    db

---
+++ teacher insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO `TEACHER` 
+    (`username`, `name` `title`, `img`, `instrument`, `place`,  `content`, `upload_date`) 
+    VALUES ('루나별', '박해영', '클래식, 뉴에이지, ost 등 책임지는 유쾌한 루나별', '/image/sample-yiruma-chaconne.png', '피아노' ,'서울특별시 관악구 신림동', '안녕하세요. 한국 예술 종합학교에 졸업한 클래식 피아노 박해영입니다.', now());
